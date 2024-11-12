3ed58e9a0dfe1a85e05400e1dbed51fb389367ef
Andreas Schlapbach <schlpbch@gmail.com>
2024-11-12 19:29:49 +0100

    feat: add new Guild42 components

---
+++ components/Guild42PatronCategoryLIFE.vue
@@ -0,0 +1,64 @@
+<template>
+  <div grid="~ cols-2 gap-2">
+    <div class="py-3 mt-2">
+      <ul>
+        <li>
+          <a href="https://www.linkedin.com/in/johann-fuchs-b48a522/">Johann Fuchs</a>
+        </li>
+        <li>
+          <a href="https://www.lightware-solutions.ch/">Lightware Solutions GmbH, Bern</a> (Andreas Pfeuti)
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/dlorch/">Daniel Lorch</a>
+        </li>
+        <li>
+          <a href="https://tim-koko.ch/blog/tim-koko/">Tim&amp;Koko AG</a> (Thomas Philipona)
+        </li>
+        <li>
+          <a href="https://sourcefactory.ch/">sourcefactory.ch</a> (Marc Bouquet, Markus Wild)
+        </li>
+        <li>
+          <a href="https://www.ims.ch/">IMS Informatik und Management Service AG</a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/markushorisberger/">Markus Horisberger</a>
+        </li>
+        <li>
+          <a href="https://www.martinelli.ch/">Martinelli GmbH</a> (Simon Martinelli)
+        </li>
+      </ul>
+    </div>
+    <div class="py-3 mt-2">
+      <ul>
+        <li>
+          <a href="https://www.linkedin.com/in/matthias-rueedlinger/">Matthias Rüedlinger</a>
+        </li>
+        <li>
+          <a href="https://www.noser.com/">Noser Engineering AG, Bern</a> (Stephan Marti)
+        </li>
+        <li>
+          <a href="https://plaintext.ch/">Plaintext GmbH</a> (Daniel Marthaler)
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/company/codeblock-gmbh/">Codeblock GmbH</a> (Claude Gex)
+        </li>
+        <li>
+          <a href="https://glue.ch/">Glue Software Engineering</a> (Igor Metz)
+        </li>
+        <li>
+          <a href="https://iterate.ch/">Iterate GmbH</a> (David Kocher, Yves Langisch)
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style>
+li {
+  list-style-type: square;
+}
+
+li::marker {
+  color: rgb(96, 165, 250);
+}
+</style>
---
+++ components/Guild42PatronCategoryUNIVERSE.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="py-3 mt-2">
+    <ul>
+      <li>
+        <a href="https://www.avega.ch/">avega IT AG, Bern</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style>
+li {
+  list-style-type: square;
+}
+
+li::marker {
+  color: rgb(96, 165, 250);
+}
+</style>
